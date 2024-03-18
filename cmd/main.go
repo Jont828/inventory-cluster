32aca5c99ef99ff53e8df4688d6d4213568bb8c1
Jont828 <jt572@cornell.edu>
2024-03-18 18:51:54 -0400

    Create InventoryCluster API type

--- cmd/main.go
+++ cmd/main.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	multiclusterxk8siov1alpha1 "inventory-cluster/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -42,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(multiclusterxk8siov1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
